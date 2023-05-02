e2f422a895e8ab8c56805e7ae92e1a92823a49ea
Arbreshë Rubovci <arbresharubovci@gmail.com>
2023-05-02 14:37:49 +0000

    Move cloudianary credentials to rails credentials

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,6 +1,6 @@
 Cloudinary.config do |config|
-  config.cloud_name = "dtnvreycq"
-  config.api_key = "374794586267358"
-  config.api_secret = "LdhRi_7s-6ashoepn09oiwx31a8"
+  config.cloud_name = Rails.application.credentials.dig(:cloudinary, :cloud_name)
+  config.api_key = Rails.application.credentials.dig(:cloudinary, :api_key)
+  config.api_secret = Rails.application.credentials.dig(:cloudinary, :api_secret)
   config.cdn_subdomain = true
 end
